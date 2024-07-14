f9fa3e15997570214a2de76e82e5a21a84b2e20a
Samir Cəbrayıllı <samirjabrailli98@gmail.com>
2024-07-14 15:43:02 +0400

    Our Tours

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Tours from './components/Tours';
+
+function App() {
+  
+  return (
+   <div>
+    <h1>Our Tours</h1>
+    <hr />
+    <Tours />
+   </div>
+  );
+}
+
+export default App
